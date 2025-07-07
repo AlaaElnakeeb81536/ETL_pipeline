1ee4318f9f70577a5459bd60db576a9e65cc2e44
Alaa El-nakeeb <alaaelnakeeb404@gmail.com>
2025-07-07 23:40:26 +0000

    commit1

---
+++ models/marts/dim_customer.sql
@@ -0,0 +1,4 @@
+{{ config(materialized="view") }}
+
+select customer_id, customer_name, email, country
+from {{ ref("stg_customer") }}
---
+++ models/marts/dim_product.sql
@@ -0,0 +1,6 @@
+select
+  product_id,
+  product_name,
+  product_category,
+  product_price
+from {{ ref('stg_product') }}
