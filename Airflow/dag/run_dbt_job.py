fb9e178b84319df5476f25af44932c881793bcc0
Alaa El-nakeeb <103367236+AlaaElnakeeb81536@users.noreply.github.com>
2025-07-08 04:16:26 +0300

    Create run_dbt_job.py

---
+++ Airflow/dag/run_dbt_job.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.providers.dbt.cloud.operators.dbt import DbtCloudRunJobOperator
+from airflow.utils.dates import days_ago
+
+with DAG(
+    dag_id="run_dbt_job",
+    schedule_interval="@daily",
+    start_date=days_ago(1),
+    catchup=False,
+    tags=["dbt"],
+) as dag:
+
+    run_dbt_job = DbtCloudRunJobOperator(
+        task_id="run_dbt_job",
+        dbt_cloud_conn_id="my_conn",  # ← غيريها لاسم الكونكشن بتاعك
+        job_id=YOUR_DBT_JOB_ID,       # ← اكتبي هنا ID بتاع الـ job من dbt Cloud
+        check_interval=10,
+        timeout=300,
+    )
