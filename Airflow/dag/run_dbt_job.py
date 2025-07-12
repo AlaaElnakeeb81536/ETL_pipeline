d531a20f5b9931efe3a70dc7df8c7b43804981ec
Alaa El-nakeeb <103367236+AlaaElnakeeb81536@users.noreply.github.com>
2025-07-12 19:42:51 +0300

    Update run_dbt_job.py

--- Airflow/dag/run_dbt_job.py
+++ Airflow/dag/run_dbt_job.py
@@ -12,8 +12,8 @@ with DAG(
 
     run_dbt_job = DbtCloudRunJobOperator(
         task_id="run_dbt_job",
-        dbt_cloud_conn_id="my_conn",  # ← غيريها لاسم الكونكشن بتاعك
-        job_id=YOUR_DBT_JOB_ID,       # ← اكتبي هنا ID بتاع الـ job من dbt Cloud
+        dbt_cloud_conn_id="my_conn",  
+        job_id=YOUR_DBT_JOB_ID,      
         check_interval=10,
         timeout=300,
     )
